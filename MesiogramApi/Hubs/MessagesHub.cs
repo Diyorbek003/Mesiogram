82633c0a8048a603136f53d4e2814b57eb4b39e6
Diyorbek003 <jaloldinusmonov609@gmail.com>
2023-03-13 17:17:46 +0500

    Run qilishga tayyor

--- MesiogramApi/Hubs/MessagesHub.cs
+++ MesiogramApi/Hubs/MessagesHub.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 
-namespace MesiogramApi.Hubs
+namespace MesiogramApi.Hubs;
+
+public class MessagesHub : Hub// royhattan otkan clentlarni ozida saqlaydi list bilan 
 {
-    public class MessagesHub : Hub
+    public async Task SendMessage(string message, string username)
     {
+        await Clients.All.SendAsync("GetMessage",message, username);
     }
 }
--- MesiogramApi/Program.cs
+++ MesiogramApi/Program.cs
@@ -24,5 +24,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.MapHub<MessagesHub>("/messages");
-
 app.Run();
